cc7e9d17d955f1d2971d50463fdeee42bceb1c7e
Madhuraj38 <kmadhuraj3080@gmail.com>
2024-10-18 12:11:41 -0400

    added us counties map

--- src/components/GeoMap.js
+++ src/components/GeoMap.js
@@ -1,11 +1,147 @@
-import React from 'react'
+import React from 'react';
+import * as d3 from "d3";
+import '../App.css';
+import Map from "./Map"
+import MapLegend from './MapLegend';
 
-function GeoMap() {
-  return (
-    <div>
-        <h1>GeoMap is still in develpment</h1>
-    </div>
-  )
+export default class GeoMap extends React.Component {
+
+  state={              
+    mapBox: { width: 100, height: 100, top: 0, left: 0},
+    // countyBox: { width: 100, height: 100, top: 0, left: 0},
+    // patternInfoBox: { width: 100, height: 100, top: 0, left: 0},
+    // patternBox: { width: 100, height: 100, top: 0, left: 0}, 
+    showInfo: false,
+    infoText: null,
+    mapData: null,
+    selectedCounty: null,
+    selectedCountyDetails: null,
+    scaleFactor: 1 
+  }
+
+  componentDidMount() {
+    var headerH = 50;
+    let winWidth = window.innerWidth;
+    let winHeight = window.innerHeight - headerH;
+    var pad = 8;
+    var scaleInfo = 1;
+    if( window.innerHeight < 650) {
+      scaleInfo = 0.75;
+    }
+    
+
+    this.setState({            
+                    mapBox:
+                      { 
+                        width: 0.55*(winWidth - pad*3), 
+                        height: 0.75*(winHeight-3*pad), 
+                        top: pad + headerH, 
+                        left: pad
+                      }
+                  });
+
+    this.loadData();
+
+    window.addEventListener('resize', this.onResize.bind(this), false);
+  }
+
+  onResize (){
+    var headerH = 50
+    let winWidth = window.innerWidth;
+    let winHeight = window.innerHeight - headerH;
+    var pad = 8
+    var scaleInfo = 1
+    if( window.innerHeight < 650) {
+      scaleInfo = 0.75
+    }
+
+    this.setState({     
+                    headerBox:
+                    { 
+                      width: winWidth, 
+                      height: headerH, 
+                      top: 0, 
+                      left: 0
+                    },               
+                  mapBox:
+                    { 
+                      width: 0.55*(winWidth - pad*3), 
+                      height: 0.75*(winHeight-3*pad), 
+                      top: pad + headerH, 
+                      left: pad
+                    },
+                  countyBox:
+                    { 
+                      width: 0.45*(winWidth - pad*3), 
+                      height:  0.6*(0.75*(winHeight-3*pad) - pad), 
+                      top: pad + headerH, 
+                      left: 0.55*(winWidth - pad*3) + 2*pad
+                    },
+                  patternInfoBox:
+                    { 
+                      width: 0.45*(winWidth - pad*3), 
+                      height: 0.4*(0.75*(winHeight-3*pad) - pad), 
+                      top: 0.6*(0.75*(winHeight-3*pad) - pad) + 2*pad + headerH,
+                      left: 0.55*(winWidth - pad*3) + 2*pad
+                    },
+                  patternBox:
+                    { 
+                      width: winWidth - 2*pad, 
+                      height: 0.25*(winHeight - 3*pad), 
+                      top: 0.75*(winHeight-3*pad) + 2*pad + headerH, 
+                      left: pad
+                    },
+                  infoBox:
+                    { 
+                      width: winWidth < 460 ? winWidth : 460, 
+                      maxHeight: winHeight / 2, 
+                      top: winHeight < 350 ? pad : winHeight/2 - 175, 
+                      left: winWidth < 460 ? pad : winWidth/2 - 230,
+                      background: "white",
+                      paddingBottom: 30
+                    },
+                    scaleFactor: scaleInfo,
+                  });
+  }
+  
+  loadData() {
+    console.log("inside load");
+    let setState = this.setState.bind(this);
+    d3.json("data/us.json").then(function(mapData) {  
+      console.log("inside d3");   
+      setState({mapData: mapData}); 
+      console.log(mapData);
+      // d3.json("data/county_stats_interpreted.json", function(countyStats) {  
+      //         setState({mapData: mapData, countyStats: countyStats});
+      // });    
+    }).catch(function(error){
+      console.log("error :",error);
+    });
+  }
+
+  render(){ 
+    return (
+      <div>
+        <div className="contentdiv" style={this.state.mapBox}>
+            <label class="contendDivHead">Map</label>          
+            {/* <MapLegend width={200} height={35}></MapLegend> */}
+            {
+              
+              this.state.mapData == null
+              ? null
+              : <Map width={this.state.mapBox.width-10} height={this.state.mapBox.height-50} padding={10} data={this.state.mapData} ></Map>
+            }
+            {/* {
+              this.state.selectedCounty == null & this.state.selectedPattern == null
+              ? null
+              : <input className="clear" type="button" value="Reset Selections" onClick={this.resetSelections.bind(this)} />
+            } */}
+          </div>
+      </div>
+      // <div>
+      //     <h1>GeoMap is still in develpment</h1>
+      // </div>
+    );
+  }
 }
 
-export default GeoMap
---
+++ src/components/Map.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import * as d3 from "d3";
+import { geoPath } from "d3-geo"
+import { feature } from "topojson-client"
+import {schemeBlues} from "d3-scale-chromatic"
+
+const projection = null
+
+const renderCounty = () => {    
+  return (d, index) => {
+    
+    // var no_death = deaths[+d.id].val 
+    const pathProps = {
+      key: `path-${ index }`,
+      d: d.path,            
+      stroke: "#ffffff",
+      strokeWidth: 0.5, 
+      cursor: "pointer"
+    } 
+
+    // if(patternStates.length > 0) {
+    //   pathProps.fill = "#f1f1f1"      
+    //   if(patternStates.includes(+d.id)){
+    //      pathProps.fill =  colorScale(+no_death)        
+    //   }
+    // }
+    // else{
+    //   pathProps.fill =  colorScale(+no_death)
+    // }
+    /*onClick={() => onSelect(d, deaths[+d.id])}><title>{"County: " + deaths[+d.id].county + "\nState: " + deaths[+d.id].state + "\nDeaths: " + no_death + " per 100K"}</title>*/
+    return <path {...pathProps} ></path>
+  };
+};
+
+const renderState = () => {    
+  return (d, index) => {
+    
+    const pathProps = {
+      key: `st-path-${ index }`,
+      d: d.path,      
+      fill: "none",
+      stroke: "#ffffff",//"#cecece",
+      strokeWidth: 2, 
+    }      
+    return <path {...pathProps} ></path>
+  };
+};
+
+export default class Map extends React.Component {
+  svgRef = React.createRef();
+  state = {
+    transform: `translate(${0}, ${0}) scale(${1})`,
+    height:200,
+    width: 300,
+    scale: 1,
+    countyPaths: [],
+    statePaths: [],
+    selectedCounty: null,
+  }
+
+  componentDidMount() {
+    var wd = this.props.width
+    var ht = this.props.height
+    this.zoom = d3
+      .zoom()
+      .scaleExtent([0.8, 7])
+      .translateExtent([
+        [-100, -100],
+        [wd + 100, ht + 100],
+      ])
+      .extent([
+        [-100, -100],
+        [wd + 100, ht + 100],
+      ])
+      .on("zoom", this.zoomed.bind(this));
+    d3.select(this.svgRef.current).call(this.zoom);
+
+    var ft = feature(this.props.data, this.props.data.objects.counties).features
+    var countyPaths = ft.map( (d, index) => { return { id: d.id, path: geoPath().projection(projection)(d)} })
+
+    var ft2 = feature(this.props.data, this.props.data.objects.states).features
+    var statePaths = ft2.map( (d, index) => { return { id: d.id, path: geoPath().projection(projection)(d)} })
+
+    var w = this.props.width
+    var h = this.props.height 
+    var l = 0
+    var s = w / 1000 > h / 585 ? h / 585 : w / 1000;
+    
+    this.setState({countyPaths: countyPaths, statePaths: statePaths, transform: `translate(${l}, ${0}) scale(${s})`, scale: s, height:h, width: w})
+  }
+  
+  static getDerivedStateFromProps(props, state){
+    if(state.width != props.width || state.height != props.height) {
+      var w = props.width
+      var h = props.height 
+      var l = 0
+      var s = w / 1000 > h / 586 ? h / 586 : w / 1000;
+      
+      return({transform: `translate(${l}, ${0}) scale(${s})`, scale: s, height:h, width: w})
+    }
+    if(props.selectedCounty == null){
+      return({selectedCounty: null});
+    }
+    return null
+  }
+
+  zoomed(event) {
+    var zoomTransform = event.transform
+    var x = 0
+    var y = 0
+    var transform = `translate(${x + zoomTransform.x}, ${y + zoomTransform.y}) scale(${this.state.scale*zoomTransform.k})`;
+    this.setState({transform: transform})   
+  }
+
+  onSelect(item, details) {
+    this.props.onSelect(+item.id, details);
+    this.setState({selectedCounty: item.path})
+  }
+
+  render() {
+      
+    var colorScale = d3.scaleThreshold().domain(d3.range(0, 160, 20)).range(schemeBlues[9]);
+    
+    return (
+        <svg width={this.props.width} height={this.props.height} style={{margin:5}} ref="svg"  >
+          <g className="country"></g>
+          
+          <g className="counties" transform={this.state.transform}>
+            {this.state.countyPaths.map(renderCounty())}
+          </g>
+          <g className="states" transform={this.state.transform}>
+            {this.state.statePaths.map(renderState())}
+            <path d={this.state.selectedCounty} fill={"none"} stroke={"#eb9514"} strokeWidth={1.5} ></path>
+          </g>
+        </svg>
+    );
+  }
+}
